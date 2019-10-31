29cf787de9776514c10427fe3f240640bd53f34f
Вячеслав Юрасов <2665603@gmail.com>
2019-10-31 21:48:45 +0500

    Базовый релиз

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace FileSpy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox_Log = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button_ClearLog = new System.Windows.Forms.Button();
+            this.textBox_TargetPath = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.наблюдаемыйКаталогToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox_Log
+            // 
+            this.textBox_Log.Location = new System.Drawing.Point(12, 79);
+            this.textBox_Log.Multiline = true;
+            this.textBox_Log.Name = "textBox_Log";
+            this.textBox_Log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_Log.Size = new System.Drawing.Size(483, 287);
+            this.textBox_Log.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // button_ClearLog
+            // 
+            this.button_ClearLog.Location = new System.Drawing.Point(420, 372);
+            this.button_ClearLog.Name = "button_ClearLog";
+            this.button_ClearLog.Size = new System.Drawing.Size(75, 23);
+            this.button_ClearLog.TabIndex = 2;
+            this.button_ClearLog.Text = "CLEAR LOG";
+            this.button_ClearLog.UseVisualStyleBackColor = true;
+            this.button_ClearLog.Click += new System.EventHandler(this.Button_Click_ClearLog);
+            // 
+            // textBox_TargetPath
+            // 
+            this.textBox_TargetPath.Location = new System.Drawing.Point(12, 27);
+            this.textBox_TargetPath.Name = "textBox_TargetPath";
+            this.textBox_TargetPath.Size = new System.Drawing.Size(483, 20);
+            this.textBox_TargetPath.TabIndex = 3;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(511, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.наблюдаемыйКаталогToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.menuToolStripMenuItem.Text = "Настройки";
+            // 
+            // наблюдаемыйКаталогToolStripMenuItem
+            // 
+            this.наблюдаемыйКаталогToolStripMenuItem.Name = "наблюдаемыйКаталогToolStripMenuItem";
+            this.наблюдаемыйКаталогToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.наблюдаемыйКаталогToolStripMenuItem.Text = "Наблюдаемый каталог";
+            this.наблюдаемыйКаталогToolStripMenuItem.Click += new System.EventHandler(this.НаблюдаемыйКаталогToolStripMenuItem_Click);
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(201, 22);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.ОПрограммеToolStripMenuItem_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 407);
+            this.Controls.Add(this.textBox_TargetPath);
+            this.Controls.Add(this.button_ClearLog);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox_Log);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_Log;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button_ClearLog;
+        private System.Windows.Forms.TextBox textBox_TargetPath;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem наблюдаемыйКаталогToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FileSpy
+{
+    public partial class Form1 : Form
+    {
+        public readonly string logString = " (" + DateTime.Now.ToString("dd.MM.yyyy (HH:mm)") + ")" + "\r\n"; //временная строка для лога
+        public string dirPath, filePath;
+        private List<string> default_FileCollection = new List<string>(); //дефолтный список файлов
+        private List<string> changed_FileCollection = new List<string>(); //измененный список файлов
+
+        public Form1()
+        {
+            InitializeComponent();
+            //подготовка для иконки в трее
+            notifyIcon1.Visible = false;
+            this.notifyIcon1.MouseDoubleClick += new MouseEventHandler(notifyIcon1_MouseDoubleClick);  //событие при двойном клике на иконку в трее
+            this.Resize += new System.EventHandler(this.Form1_Resize); //событие при изменение размера окна в часности свертывания
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "FileSpy v1.1 (c) Юрасов В.В.";
+            ReadSettings(); //получаем первоначальные настройки
+            dirPath = textBox_TargetPath.Text;
+            filePath = @"log.txt";
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo dir;
+            try { dir = new DirectoryInfo(textBox_TargetPath.Text); }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+            if (!dir.Exists) //проверяем существует ли выбранный каталог
+            {
+                MessageBox.Show("Выбранного каталога не существует!"); return;
+            }
+            dirPath = textBox_TargetPath.Text;
+            SaveSettings();
+            default_FileCollection = ListGenerator(); //получаем список файлов
+
+            if (timer1.Enabled == false)
+            {
+                button1.Text = "STOP";
+                timer1.Enabled = true;
+                textBox_TargetPath.Enabled = false;
+                return;
+            }
+            if (timer1.Enabled == true)
+            {
+                button1.Text = "START";
+                timer1.Enabled = false;
+                textBox_TargetPath.Enabled = true;
+                return;
+            }
+
+        }
+        private void Comprase(List<string> base_FileCollection) //сравнием старый и новый список
+        {
+            changed_FileCollection = ListGenerator(); //получаем новый список файлов
+            if (changed_FileCollection.SequenceEqual(base_FileCollection)) return; //если список не изменился то ничего не делаем
+
+            if (changed_FileCollection.Count > base_FileCollection.Count) //если файлов стало больше (добавление)
+            {
+                var result = changed_FileCollection.Except(base_FileCollection); //формируем список из новых файлов
+                foreach (var i in result) Logging("+ " + i + logString); //логирум
+            }
+            if (changed_FileCollection.Count < base_FileCollection.Count) //если файлов стало меньше (удаление)
+            {
+                var result = base_FileCollection.Except(changed_FileCollection); //формируем список из пропавших файлов
+                foreach (var i in result) Logging("- " + i + logString); //логируем
+            }
+            default_FileCollection = changed_FileCollection; //обновляем дефорлтный список файлов
+        }
+        private void Logging(string save) //Логирование путем добавления в TextBox и записи в файл лога
+        {
+            textBox_Log.Text += save;
+            textBox_Log.SelectionStart = textBox_Log.Text.Length; textBox_Log.ScrollToCaret(); //перемотка в конец
+            using (StreamWriter sw = new StreamWriter("log.txt", true, System.Text.Encoding.GetEncoding(1251)))
+            {
+                sw.Write(save);
+            }
+        }
+
+        public List<string> ListGenerator() //получает список файлов из указаной папки
+        {
+            List<string> list = new List<string>();
+            DirectoryInfo dir = new DirectoryInfo(dirPath);
+            FileInfo[] fileColection = dir.GetFiles(); //получаем набор файлов в папке
+            foreach (var f in fileColection)
+                list.Add(f.Name);
+            return list;
+        }
+
+        private void Button_Click_ClearLog(object sender, EventArgs e) //отчистка файла с логами и текстбокса
+        {
+            textBox_Log.Clear();
+            using (StreamWriter sw = new StreamWriter(filePath, false, System.Text.Encoding.GetEncoding(1251)))
+            {
+                try
+                {
+                    sw.WriteLine();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            }
+        }
+        private void Timer1_Tick(object sender, EventArgs e) //проверка каталога по таймеру
+        {
+            Comprase(default_FileCollection);
+        }
+        private void SaveSettings() //сохрание настроек
+        {
+            try
+            {
+                using (BinaryWriter writer = new BinaryWriter(File.OpenWrite("settings.bin")))
+                {
+                    writer.Write(textBox_TargetPath.Text);
+                }
+            }
+            catch
+            {
+                return;
+            }
+        }
+        private void ReadSettings() //чтение настроек
+        {
+            try
+            {
+                using (BinaryReader readerData = new BinaryReader(File.OpenRead("settings.bin")))
+                {
+                    textBox_TargetPath.Text = readerData.ReadString();
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                return;
+            }
+
+        }
+
+        private void ОПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Разработано Юрасовым В.В. \r\nв 2019 году");
+        }
+        private void Form1_Resize(object sender, EventArgs e) //изменение размера окна (сворачивание)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                notifyIcon1.Visible = true;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e) //двойной клик по иконке в трее
+        {
+            this.Show();
+            notifyIcon1.Visible = false;
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void НаблюдаемыйКаталогToolStripMenuItem_Click(object sender, EventArgs e) //выбор каталога для наблюдения
+        {
+            folderBrowserDialog1.Description = "Выберите каталог для отслеживания";
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            textBox_TargetPath.Text = folderBrowserDialog1.SelectedPath;
+        }
+    }
+}
